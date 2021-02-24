07ce7c702b2a42d63557d5a707e80520759a9afc
mint <mint@insdim.com>
2021-02-24 23:08:37 +0800

    Init

--- hole.ino
+++ hole.ino
@@ -1,6 +1,15 @@
+#include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
+#include <Wire.h>
+#include <NewPing.h>
 
-#define PIN 6
+#define PIN 12
+
+#define TRIGGER_PIN 8
+#define ECHO_PIN 9
+#define MAX_DIS 400
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DIS);
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = pin number (most are valid)
@@ -15,16 +24,58 @@ void setup()
 {
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
+
+  pinMode(11, OUTPUT);
+  digitalWrite(11, HIGH);
+
+  Serial.begin(115200);
 }
 
 void loop()
 {
+  delay(100);
+  unsigned int uS = sonar.ping();
+
   // Some example procedures showing how to display to the pixels:
-  colorWipe(strip.Color(255, 0, 0), 50); // Red
-  colorWipe(strip.Color(0, 255, 0), 50); // Green
-  colorWipe(strip.Color(0, 0, 255), 50); // Blue
-  rainbow(20);
-  rainbowCycle(20);
+  if (uS >= 5000)
+  {
+    colorWipe(strip.Color(0, 167, 104), 30);
+  }
+  else if (uS >= 3000 && uS < 5000)
+  {
+    colorWipe(strip.Color(0, 47, 167), 30);
+  }
+  else if (uS >= 2000 && uS < 3000)
+  {
+    colorWipe(strip.Color(65, 0, 167), 30);
+  }
+  else if (uS >= 1000 && uS < 2000)
+  {
+    colorWipe(strip.Color(155, 0, 167), 30);
+  }
+  else if (uS >= 500 && uS < 1000)
+  {
+    colorWipe(strip.Color(167, 0, 69), 30);
+  }
+  else
+  {
+    colorWipe(strip.Color(255, 255, 0), 30);
+  }
+}
+
+void transition(uint32_t c1, uint32_t c2)
+{
+  if (c1 > c2)
+  {
+    uint32_t m = c1;
+    c1 = c2;
+    c2 = m;
+  }
+
+  for (uint32_t i = c1; i < c2; i++)
+  {
+    colorWipe(i, 10);
+  }
 }
 
 // Fill the dots one after the other with a color
